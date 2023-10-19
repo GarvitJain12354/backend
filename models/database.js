8d5933180a465794554739b28022b71871928cf1
first <garvitjainraju@gmail.com>
2023-10-19 11:56:08 +0530

    new

--- models/database.js
+++ models/database.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 
 exports.connectedDatabase = async()=>{
     try {
-        await mongoose.connect(process.env.MONGODB_URL);
+        await mongoose.connect("mongodb+srv://garvitjainraju:QHmB92NOAp4Ztl4Z@smartsolution.m7c3vvn.mongodb.net/?retryWrites=true&w=majority");
         console.log("Database connected");
     } catch (error) {
         console.log(error);
